42da39af405a539a4820f0d4a9d9d51b86a869e1
mis <mearlboro@protonmail.com>
2021-03-26 17:24:41 +0000

    Minor fix to setup script and add empty authorized_keys

--- setup/setup_sd_card.sh
+++ setup/setup_sd_card.sh
@@ -7,9 +7,9 @@ case $1 in
     '' | *[!0-9]*) echo "setup_sd_card.sh: arg not a number" & exit
 esac
 
-#if [ ! -d /media/$(whoami)/boot ] || [ ! -d /media/$(whoami)/rootfs ]; then
-#	echo "setup_sd_card.sh: make sure the Raspberry Pi drives are mounted" & exit
-#fi
+if [ ! -d /media/$(whoami)/boot ] || [ ! -d /media/$(whoami)/rootfs ]; then
+	echo "setup_sd_card.sh: make sure the Raspberry Pi drives are mounted" & exit
+fi
 
 
 number=$1
@@ -21,7 +21,7 @@ echo "setup_sd_card.sh: setting up $hostname with IP address $ipaddr"
 echo "$hostname" > etc/hostname
 sed -i "/static ip_address/s/192.168.100.[0-9][0-9][0-9]/$ipaddr/" etc/dhcpcd.conf
 
-sudo cp etc/*  /media/$(whoami)/rootfs/etc/
+sudo cp etc/* /media/$(whoami)/rootfs/etc/
 
 
 if [ ! -f .ssh/synchlive.pub ]; then
@@ -32,8 +32,9 @@ echo "setup_sd_card.sh: copying wireless network settings and SSH keys"
 
 sudo cp boot/* /media/$(whoami)/boot/
 
-mkdir rootfs/home/pi/.ssh
-cat .ssh/synchlive.pub > rootfs/home/pi/.ssh/authorized_keys
+cat .ssh/synchlive.pub > .ssh/authorized_keys
+mkdir /rootfs/home/pi/.ssh
+cp .ssh/authorized_keys /rootfs/home/pi/.ssh/authorized_keys
 
 umount /media/$(whoami)/boot
 umount /media/$(whoami)/rootfs
