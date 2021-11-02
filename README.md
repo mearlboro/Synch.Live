e06828241de5828c4990176411fcd3e441716e2d
mis <mearlboro@protonmail.com>
2021-11-02 19:06:31 +0000

    Add emergence calculator
    - add JIDT infodynamics.jar package
    - update requirements and readme

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ This branch contains development for the **observer** system equipped with the
 RPi Camera Module, as well as example media files and code that can be used
 offline for testing and analysis.
 
+Mainly the code on this branch performs object detection and object tracking to
+produce trajectories, and computes information-theoretical causal emergence of
+these trajectories using the positions of each player and the centre of mass of
+the group as described in:
+
+Rosas FE*, Mediano PAM*, Jensen HJ, Seth AK, Barrett AB, Carhart-Harris RL, et al.
+(2020) [_Reconciling emergences: An information-theoretic approach to identify
+causal emergence in multivariate data_](https://doi.org/10.1371/journal.pcbi.1008289).
+PLoS Comput Biol 16(12):e1008289.
 
 ## Contents
 
@@ -14,6 +23,7 @@ deployed via Ansible (for config, see branch `dev-ansible`):
 streaming
 - `detection.py` - use HSV filters on an OpenCV image to detect the lights of
 the players on each frame
+- `emergence_calculator.py` - calculate emergence values
 - `hsv_range.py` - tools for extracting colour ranges from an image
 - `logger.py` - logging setup to be used when the system runs live
 - `server.py` - runs a Flask app to stream the camera footage and a web control
@@ -37,30 +47,59 @@ To run parts of the code locally you must install the pkgs in `camera/requiremen
 We recommend packaging with `pipenv` and the code should be run inside a `pipenv`
 shell in the `camera/` folder
 
-    pipenv shell requirements.txt
+    $ pipenv shell requirements.txt
 
 
 ### Extracting and plotting trajectories
 
 To extract trajectorie from video using non-real time tracking use
 
-    python trajectories.py track --filename $video_file --out $traj_file
+    $ python trajectories.py track --filename $video_file --out $traj_file
 
 The extracted trajectories will be saved as a numpy dump.
 
 
 To plot the extracted trajectories use
 
-    python trajectories.py plot --filename $traj_file --out $image_file
+    $ python trajectories.py plot --filename $traj_file --out $image_file
 
 
 ### Image processing tools
 
 To inspect colours of an image, and produce HSV values of colour ranges
 
-    python hsv_range.py --filename $image_file
+    $ python hsv_range.py --filename $image_file
 
 An OpenCV GUI with sliders for hue, saturation and value for the lower and
 higher ends of the colour range to be selected from the image.
 Move sliders to replace excluded colour with black until the desired range is
 found.
+
+### Calculating emergence
+
+The code in `emergence_calculator.py` is contributed by [Dr. Pedro Mediano](https://github.com/pmediano)
+using the [Java Information Dynamics Toolkit (JIDT)](https://github.com/jlizier/jidt/)
+by [Dr. Joe Lizier](https://github.com/jlizier).
+
+To test the emergence calculator on the trajectories, run
+
+    $ python emergence_calculator.py --filename $traj_file
+
+
+The version used is a slightly modified `v1.5-dist` rebuilt with `ant` and is included in
+the `camera/` folder as `infodynamics.jar`. The JIDT code is called using JPype.
+The following steps were taken to produce our version of JIDT:
+
+    $ sudo apt install ant
+    $ git clone git@github.com:jlizier/jidt.git
+
+Then the file `java/source/infodynamics/measures/continuous/MutualInfoMultiVariateCommon.java`
+is edited to keep the vectors of observation sets in the `finaliseAddObservations()` function
+by commenting out the following lines:
+
+    vectorOfSourceObservations = null;
+    vectorOfDestinationObservations = null;
+
+Finally an `infodynamics.jar` package is built by calling
+
+    $ ant jar
---
+++ camera/emergence_calculator.py
@@ -0,0 +1,193 @@
+"""
+Simple class implementing a running calculator for the quantities related to
+the PhiID theory of causal emergence, as described in:
+
+Rosas FE*, Mediano PAM*, Jensen HJ, Seth AK, Barrett AB, Carhart-Harris RL, et
+al. (2020) Reconciling emergences: An information-theoretic approach to
+identify causal emergence in multivariate data. PLoS Comput Biol 16(12):
+e1008289.
+
+Pedro Mediano, Oct 2021
+"""
+
+import click
+import logging
+import numpy as np
+import jpype as jp
+
+# initialise logging to file
+import logger
+
+INFODYNAMICS_PATH = 'infodynamics.jar'
+SAMPLE_THRESHOLD = 10
+
+def javify(Xi):
+    """
+    Convert a numpy array into a Java array to pass to the JIDT classes and
+    functions.
+    Given a 1-dimensional np array of shape (D,)  , return Java array of size D
+    Given a 2-dimensional np array of shape (1, D), return Java array of size D
+
+    Params
+    ------
+    Xi
+        numpy array of shape (D,) or (1,D) representing one 'micro' part of the
+        system or one value of the macroscopic feature
+    """
+    if len(Xi.shape) == 1:
+        D = Xi.shape[0]
+        Xi = Xi[np.newaxis, :]
+        jXi = jp.JArray(jp.JDouble, D)(Xi.tolist())
+    else:
+        D = Xi.shape[1]
+        jXi = jp.JArray(jp.JDouble, D)(Xi.tolist())
+
+    return jXi
+
+
+def compute_macro(X):
+    """
+    Computes a supervenient macroscopic feature.
+
+    Parameters
+    ----------
+    X : iter of np.ndarray
+        Each element of X contains a 1D np.ndarray representing one 'micro'
+        part of the system.
+
+    Returns
+    -------
+    V : np.ndarray
+        Macroscopic feature of interest of D-dimensions of shape (1,D)
+    """
+    V = np.mean(X, axis = 0)
+
+    return V[np.newaxis, :]
+
+
+class EmergenceCalculator():
+    def __init__(self, use_correction=True):
+        """
+        Construct the emergence calculator by setting member variables and
+        checking the JVM is started. The JIDT calculators will be initialised
+        later, when the first batch of data is provided.
+
+        Parameters
+        ----------
+        use_correction : bool
+            Whether to use the 1st-order lattice correction for emergence
+            calculation.
+        """
+
+        self.past_X = None
+        self.past_V = None
+
+        self.is_initialised = False
+        self.sample_counter = 0
+
+        self.use_correction = use_correction
+
+        if not jp.isJVMStarted():
+            logging.info('Starting JVM...')
+            jp.startJVM(jp.getDefaultJVMPath(), '-ea', '-Djava.class.path=%s'%INFODYNAMICS_PATH)
+            logging.info('JVM started using jpype1')
+
+        logging.info('Successfully initialised EmergenceCalculator')
+
+
+    def initialise_calculators(self, X, V):
+        """
+        """
+        self.N = len(X)
+
+        self.xmiCalcs = []
+        for Xi in X:
+            Xi = Xi[np.newaxis, :]
+            self.xmiCalcs.append(jp.JClass('infodynamics.measures.continuous.gaussian.MutualInfoCalculatorMultiVariateGaussian')())
+            self.xmiCalcs[-1].initialise(Xi.shape[1], V.shape[1])
+            self.xmiCalcs[-1].startAddObservations()
+
+        self.vmiCalc = jp.JClass('infodynamics.measures.continuous.gaussian.MutualInfoCalculatorMultiVariateGaussian')()
+        self.vmiCalc.initialise(V.shape[1], V.shape[1])
+        self.vmiCalc.startAddObservations()
+
+        self.is_initialised = True
+
+
+    def update_calculators(self, V):
+        """
+        """
+        jV = javify(V)
+        self.vmiCalc.addObservations(javify(self.past_V), jV)
+
+        for Xip,calc in zip(self.past_X, self.xmiCalcs):
+            calc.addObservations(javify(Xip), jV)
+
+
+    def compute_psi(self, V):
+        """
+        """
+        self.vmiCalc.finaliseAddObservations()
+        jV = javify(V)
+
+        psi = self.vmiCalc.computeLocalUsingPreviousObservations(
+                javify(self.past_V), jV)[0]
+        for Xip,calc in zip(self.past_X, self.xmiCalcs):
+            calc.finaliseAddObservations()
+            psi -= calc.computeLocalUsingPreviousObservations(javify(Xip), jV)[0]
+
+        if self.use_correction:
+            marginal_mi = [ calc.computeAverageLocalOfObservations()
+                            for calc in self.xmiCalcs ]
+            psi += (self.N - 1) * np.min(marginal_mi)
+
+        return psi
+
+
+    def update_and_compute(self, X):
+        """
+        """
+        V = compute_macro(X)
+
+        psi = 0.
+        if not self.is_initialised:
+            self.initialise_calculators(X, V)
+
+        else:
+            self.update_calculators(V)
+            if self.sample_counter > SAMPLE_THRESHOLD:
+                psi = self.compute_psi(V)
+
+        self.past_X = X
+        self.past_V = V
+        self.sample_counter += 1
+
+        return psi
+
+
+@click.command()
+@click.option('--filename',  help = 'Numpy array dump of trajectories', required = True)
+@click.option('--threshold', help = 'Number of timesteps to wait before calculation')
+def test(filename, threshold = SAMPLE_THRESHOLD):
+    """
+    Test the emergence calculator on the trajectories specified in `filename`.
+
+    Params
+    ------
+    filename
+        path to a numpy dump of the trajectories (microscopic) features of the system
+    threshold
+        number of timesteps to wait before calculation, at least as many as the
+        dimension of the system
+    """
+    calc = EmergenceCalculator()
+
+    X = np.load(filename, allow_pickle=True)
+    for i in range(len(X)):
+        psi = calc.update_and_compute(X[i])
+        if psi:
+            print(f't{i}: {psi}')
+
+
+if __name__ == "__main__":
+    test()
