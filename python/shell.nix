70dfa1e132cb8875471ec451581becca7df061ac
LockwoodChris <chrislockwood@protonmail.com>
2022-03-09 15:31:15 +0000

    fix(nix): update mach-nix to v3.4.0

--- python/shell.nix
+++ python/shell.nix
@@ -1,8 +1,8 @@
 let
   mach-nix = import (builtins.fetchGit {
     url = "https://github.com/DavHau/mach-nix/";
-    rev = "31b21203a1350bff7c541e9dfdd4e07f76d874be"; # master
-    #ref = "refs/tags/3.3.0";
+    #rev = "31b21203a1350bff7c541e9dfdd4e07f76d874be"; # master
+    ref = "refs/tags/3.4.0";
   }) {
     python = "python39Full";
   };
