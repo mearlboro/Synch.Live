cf40d79798a890a5a23aa2989e2a1ee6b5455d8a
mis <mearlboro@protonmail.com>
2022-03-10 16:48:40 +0000

    WIP: run tracker in real time when checking fitness
    - add EuclideanFitness measure which returns average pair-wise distance
    - track function expanded to run detector, pair the first detected results
      to the expected results, then run tracker
    - TODO: handle fewer than N detections in one frame

--- python/camera/tools/traj_fitness.py
+++ python/camera/tools/traj_fitness.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
+from abc import ABC, abstractmethod
+import cv2
 import click
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import distance as dist
 from scipy.optimize import linear_sum_assignment
-
+import time
 from typing import List, Tuple
 
-class TrajFitnessFunction:
-    @classmethod
+from camera.core.detection import detect_colour
+from camera.core.tracking import EuclideanMultiTracker
+
+class TrajFitnessFunction(ABC):
+    @abstractmethod
     def fit(X: np.ndarray, Y: np.ndarray) -> float:
         """
         Calculate how well the approximate trajectories in Y fit with the real
@@ -24,11 +28,23 @@ class TrajFitnessFunction:
         """
         pass
 
+class EuclideanFitness(TrajFitnessFunction):
+	"""
+	Compute fitness between two sets of trajectories by computing the euclidean
+	distance between them. The sets are ordered, i.e. Xi is the expected position
+	tracked in Yi. The purpose of this function is to test tracking.
+	"""
+	@staticmethod
+	def fit(X: np.ndarray, Y: np.ndarray) -> float:
+		dists = [ dist.euclidean(xi, yi) for xi, yi in zip(X, Y) ]
+		return np.mean(dists)
 
 class HungarianFitness(TrajFitnessFunction):
     """
     Compute fitness between two sets of trajectories using the minimal distance
-    function in the Hungarian algorithm
+    function in the Hungarian algorithm. The sets are not necessarily ordered,
+	i.e. the expected position Xi is not necessarily tracked in Yi. The purpose
+	of this function is to test object detection.
     """
     @staticmethod
     def fit(X: np.ndarray, Y: np.ndarray) -> Tuple[int, float]:
@@ -41,22 +57,69 @@ class HungarianFitness(TrajFitnessFunction):
         return cost, miss
 
 
-@click.command()
-@click.option('--fx', help = 'Numpy array dump of expected trajectories', required = True)
-@click.option('--fy', help = 'Numpy array dump of approximate trajectories', required = True)
-@click.option('--fitness', help = 'Fitness function to use when verifying the tracker works')
-def track(fx: str, fy: str, fitness: str) -> None:
 
+@click.command()
+@click.option('--fvid', help = 'Video file including particles to be tracked', required = True)
+@click.option('--fx',   help = 'Numpy array dump of expected trajectories (precomputed on the same video file)', required = True)
+@click.option('--model', help = 'Motion model to use in the tracker',
+						 type =  click.Choice(['Constant', 'Kalman']))
+def track(fvid: str, fx: str, model: str) -> None:
+    """
+    Evaluate quality of EuclideanMultitracker using model specified in `model`
+    using euclidean distance
+    """
     X = np.load(fx, allow_pickle = True)
     (Tx, Nx, Dx) = X.shape
-    Y = np.load(fy, allow_pickle = True)
+
+    vid = object()
+    try:
+        vid = cv2.VideoCapture(fvid)
+        time.sleep(0.1)
+    except:
+        print("Error reading video")
+        exit(1)
+
+    success, frame = vid.read()
+    if not success:
+        print("Error reading video")
+        exit(1)
+
+    tracker = EuclideanMultiTracker()
+    Y = list()
+    boxes = detect_colour(frame)
+    Y0 = [ [ x + w / 2, y + h / 2] for (x, y, w, h) in boxes ]
+
+    print("Making sure the expected and detected coordinates are in the same order")
+    rows, cols = linear_sum_assignment(dist.cdist(X[0], Y0))
+    if not all(rows == cols):
+        print(f"Reordering {rows} to {cols}")
+        boxes_copy = boxes
+        for (r, c) in zip(rows, cols):
+             boxes[r] = boxes_copy[c]
+
+    print(f"Running real-time multitracker on video {fvid}")
+    while vid.isOpened():
+        success, frame = vid.read()
+        if not success:
+            print("Video finished")
+            break
+
+        boxes = detect_colour(frame)
+        newBoxes = tracker.update(boxes)
+        cmass = [ np.array([x + w / 2, y + h / 2]) for (x, y, w, h) in boxes ]
+        print(len(cmass))
+        Y.append(cmass)
+
+    vid.release()
+
+    Y = np.array(Y)
     (Ty, Ny, Dy) = Y.shape
 
     assert(Tx == Ty and Nx == Ny and Dx == Dy)
 
-    cs, ms = zip(*[ HungarianFitness.fit(x, y) for x, y in zip(X, Y) ])
+    cs, ms = zip(*[ EuclideanFitness.fit(x, y) for x, y in zip(X, Y) ])
 
-    plt.title('Trajectory fitness using the Hungarian algorithm')
+    plt.title('Trajectory fitness')
     plt.plot(range(Tx), cs, label = 'Cost function of tracker', color = 'r')
     plt.plot(range(Tx), ms, label = 'Number of players missed by tracker')
     plt.legend()
@@ -68,7 +131,8 @@ def options():
     pass
 
 options.add_command(track)
+# options.add_command(detect) TODO:
 
 if __name__ == '__main__':
-    options()
+    track()
 
