8ae7bfa0fea42cfaff6ecd40bad03644142889f2
Tejinder Soomal <tss122@ic.ac.uk>
2023-02-19 22:35:54 +0000

    Implemented app.routes for buttons.

--- python/leds/server/server.py
+++ python/leds/server/server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from python.leds.ws2801_headset import WS2801Headset
 import os
 import logging
+import python.leds.logger
+
 def create_app(server_type):
     app = Flask(__name__)
     app.debug = True
@@ -17,7 +19,21 @@ def create_app(server_type):
     def pilotButton():
         WS2801Headset.pilot()
 
+    @app.route('/rainbowButton')
+    def pilotButton():
+        WS2801Headset.crown_rainbow()
 
+    @app.route('/exposureButton')
+    def pilotButton():
+        return
+
+    @app.route('/breatheButton')
+    def pilotButton():
+        WS2801Headset.crown_breathe()
+
+    @app.route('/stopButton')
+    def pilotButton():
+        WS2801Headset.all_off()
 
 
 if __name__ == '__main__':
