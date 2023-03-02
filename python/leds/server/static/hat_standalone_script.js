6b90593c45c6c68beb641706728d61702384b0d8
Tejinder Soomal <tss122@ic.ac.uk>
2023-03-02 14:49:31 +0000

    Tried to go back to Last Checkpoint

--- python/leds/server/static/hat_standalone_script.js
+++ python/leds/server/static/hat_standalone_script.js
@@ -17,20 +17,27 @@ function openTab(evt, tabName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
 
-  const blickfrequencySlider = document.getElementById("blickfrequency");
-  const blickfrequencyValue = document.getElementById("blickfrequency-value");
-  blickfrequencyValue.innerHTML = blickfrequencySlider.value;
 
-  blickfrequencySlider.oninput = function() {
-    blickfrequencyValue.innerHTML = this.value;
-  };
-
-  const effectdurationSlider = document.getElementById("effectduration");
-  const effectdurationValue = document.getElementById("effectduration-value");
-  effectdurationValue.innerHTML = effectdurationSlider.value;
-
-  effectdurationSlider.oninput = function() {
-    effectdurationValue.innerHTML = this.value;
-  };
+//function changedColor() {
+//    var newRGBHex = document.getElementById("colorPicker").value;
+//    var newRGBValues = hexToRgb(newRGBHex);
+//    const request = new XMLHttpRequest();
+//    request.open('POST', '/ProcessNewColor/${JSON.stringify(newRGBValues)}');
+//    request.send();
+//    window.alert(newRGBValues);
+//}
+//
+//function hexToRgb(hex){
+//    var c;
+//    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+//        c= hex.substring(1).split('');
+//        if(c.length== 3){
+//            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+//        }
+//        c= '0x'+c.join('');
+//        return [(c>>16)&255, (c>>8)&255, c&255].join(',');
+//    }
+//    throw new Error('Bad Hex');
+//}
