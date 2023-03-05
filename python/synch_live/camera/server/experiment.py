71fe962a1930bb321c3c7aed3d5eeb0d6f689507
ambergorzynski <amber.gorzynski@gmail.com>
2023-03-05 12:24:32 +0000

    include manPsi buttons; fix frame_id

--- python/synch_live/camera/server/experiment.py
+++ python/synch_live/camera/server/experiment.py
@@ -1,18 +1,36 @@
 from flask import Blueprint, render_template, request
+from wtforms import Form, BooleanField, IntegerRangeField
+from wtforms.widgets import html_params
 
 from synch_live.camera.video.proxy import VideoProcessorClient
 
 bp = Blueprint('experiment', __name__, url_prefix='/experiment')
 
 
-@bp.route('/observe')
+@bp.route('/observe', methods=['GET','POST'])
 def observe():
+
+    form = ManualSettings(request.form)
+    form.manPsi(min=0, max=10)
+
     if request.method == "POST":
         psi = int(request.form.get("manPsi"))
-        use_psi = request.form.get("psi")
+        
+        if request.form.get("psi"):
+            use_psi = 1
+        else:
+            use_psi = 0
 
         if use_psi:
             VideoProcessorClient().task = 'emergence'
+            # writing sigmoids to database
+            VideoProcessorClient().sync
+
         else:
             VideoProcessorClient().psi = psi
-    return render_template('observe.html')
+    return render_template('observe.html', form=form)
+
+class ManualSettings(Form):
+    psi = BooleanField('Psi')
+    manPsi = IntegerRangeField('manPsi')
+
--- python/synch_live/camera/video/__init__.py
+++ python/synch_live/camera/video/__init__.py
@@ -492,6 +492,9 @@ class VideoProcessor():
         # TO DO: Does not execute if called after closing video stream due to threading issues
         write_in_experiment_parameters_end_time(self.experiment_id)
 
+        # updating frame id at the end of the experiment
+        self.frame_id = 0
+
         if self.video_stream:
             logging.info('Closing video streamer...')
             self.video_stream.stop()
