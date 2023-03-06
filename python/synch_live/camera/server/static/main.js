cc30f8b862e1289b72d3099738bb64da0cff187f
Justin Malčić <j.malcic@me.com>
2023-03-06 01:34:06 +0000

    Add flash controller

--- python/synch_live/camera/server/static/main.js
+++ python/synch_live/camera/server/static/main.js
@@ -1,3 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 
+import FlashController from "./controllers/flash_controller.js"
+
 window.Stimulus = Application.start()
+Stimulus.register("flash", FlashController)
