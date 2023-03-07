d8df673636915bd72d8a00ab89a464fd16c76a11
Justin Malčić <j.malcic@me.com>
2023-03-07 13:39:11 +0000

    Fix download

--- python/synch_live/camera/server/download.py
+++ python/synch_live/camera/server/download.py
@@ -5,33 +5,30 @@ from synch_live.camera.db import process_query, get_all_experiment_ids_query
 
 bp = Blueprint('download', __name__, url_prefix='/download')
 
-@bp.route('/get_data', methods=['GET','POST'])
-def get_data():
-    
-    #form = DataInfoForm(request.form)
-    id_list = get_all_experiment_ids_query()
-    form = DataInfoDropdown(request.form)
-    form.experiment_id.choices = id_list
-    
-    if request.method == 'POST' and form.validate():
 
+@bp.route('/get_data')
+def get_data():
+    form = DataInfoDropdown(request.args)
+
+    if form.experiment_id.data and form.validate():
         experiment_id = form.experiment_id.data        
 
         response = process_query(experiment_id)
 
         response = make_response(response.getvalue())
-        filename = "SynchLive" + experiment_id
-        response.headers['Content-Disposition'] = 'attachment; filename=' + filename + '.csv'
+        filename = f"SynchLive{experiment_id}"
+        response.headers['Content-Disposition'] = f'attachment; filename={filename}.csv'
         response.headers["Content-type"] = "text/csv"
 
         return response
 
-    #return render_template('data.html', form=form)
     return render_template('data.html', form=form)
+
 
 class DataInfoForm(Form):
     experiment_id = StringField('Experiment ID')
 
+
 class DataInfoDropdown(Form):
-    experiment_id = SelectField('Experiment ID')
+    experiment_id = SelectField('Experiment ID', choices=get_all_experiment_ids_query())
 
