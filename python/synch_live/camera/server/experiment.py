73198e2e380f19a011b4e878402a257da9830cf0
ambergorzynski <amber.gorzynski@gmail.com>
2023-03-09 08:47:27 +0000

    (fix) switching between manual and emergence tasks

--- python/synch_live/camera/server/experiment.py
+++ python/synch_live/camera/server/experiment.py
@@ -10,19 +10,33 @@ bp = Blueprint('experiment', __name__, url_prefix='/experiment')
 
 @bp.route('/observe', methods=['GET', 'POST'])
 def observe():
+    
     video_processor = VideoProcessorClient()
-    form = ManualSettings(request.form)
+    
+    form = TaskSettings(request.form)
+    
     if request.method == "POST" and form.validate():
-        if form.manual_psi.data:
+        
+        # set task to emergence if emergence button is clicked
+        if form.is_emergence.data:
+            
             video_processor.task = 'emergence'
+            
             # writing sigmoids to database
             video_processor.sync
+
+        # otherwise, set task to manual with given psi
         else:
             video_processor.psi = form.manual_psi.data
-        flash('PSI updated!')
+        
+        flash('Task updated!')
+        
         return redirect(url_for('experiment.observe'))
-    return render_template('observe.html', form=form, time=time.time())
+    
+    return render_template('observe.html', form=form, time=time.time(), task = video_processor.task)
 
 
-class ManualSettings(Form):
-    manual_psi = IntegerField('Manual PSI', [validators.number_range(min=0, max=10)])
+class TaskSettings(Form):
+    manual_psi = IntegerField("Switch task to 'Manual' by entering PSI:", [validators.Optional(), validators.number_range(min=0, max=10)])
+    is_emergence = BooleanField("Switch task to 'Emergence':")
+
